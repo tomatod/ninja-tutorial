5ec272e435b3a8d4f6dd8056b988ad63832935d6
Daichi Tomaru <banaoa7543@gmail.com>
2025-01-21 22:56:47 +0900

    tutorial -> First Steps: https://django-ninja.dev/tutorial/

--- ninja_tutorial/urls.py
+++ ninja_tutorial/urls.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from ninja import NinjaAPI
-
-api = NinjaAPI()
-
-
-@api.get("/add")
-def add(request, a: int, b: int):
-    return {"result": a + b}
-
+from .api import api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
