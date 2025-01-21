3d3dee749e12ba0a075fb3b16dfdd7545647d9b7
Daichi Tomaru <banaoa7543@gmail.com>
2025-01-21 23:15:13 +0900

    Tutorial -> Parsing Input: https://django-ninja.dev/tutorial/step2/

--- ninja_tutorial/api.py
+++ ninja_tutorial/api.py
@@ -1,7 +1,18 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
 
 api = NinjaAPI()
 
+class HelloSchema(Schema):
+    name: str = "world"
+
+@api.post("/hello")
+def hello(request, data: HelloSchema):
+    return f"Hello {data.name}"
+
 @api.get("/hello")
-def hello(request):
-    return "Hello world"
+def hello(request, name: str = "world"):
+    return f"Hello {name}"
+
+@api.get("/math/{a}and{b}")
+def math(request, a: int, b: int):
+    return {"add": a + b, "multiply": a * b}
